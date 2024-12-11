489f32b4eeaacf2e9bb23f48075e0271ad1ba7c9
poorna30646 <thotipoornachandra@gmail.com>
2024-12-11 09:58:20 +0530

    Add files via upload

---
+++ datatypes.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class datatypes
+{
+    public static void main(String args[])
+    {
+    int a=12;
+    char b='a';
+    float c=3.12f;
+    System.out.println(a);
+    System.out.println(b);
+    System.out.println(c);
+    }
+}
