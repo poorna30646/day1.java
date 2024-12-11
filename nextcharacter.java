cd0e96c12c8b0cca5bf5434e92d97742e29ea57d
poorna30646 <thotipoornachandra@gmail.com>
2024-12-11 09:59:01 +0530

    Add files via upload

---
+++ nextcharacter.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class nextcharacter
+{
+    public static void main(String args[])
+    {
+    char c='a';
+    int b=c;
+    b+=1;
+    char v=(char)b;
+    System.out.println(v);
+    }
+}
