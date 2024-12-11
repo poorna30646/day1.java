66e6ef7a268a77b67e429373202243cff5ef2257
poorna30646 <thotipoornachandra@gmail.com>
2024-12-11 09:59:17 +0530

    Add files via upload

---
+++ nextmethod.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class nextmethod
+{
+    public static void main(String args[])
+    {
+    Scanner sc=new Scanner(System.in);
+    String a=sc.next();
+    System.out.println("congratulations "+a+" you won 5 lakhs");
+    }
+
+}
