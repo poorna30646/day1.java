ae192a53cd28333088f31c85abe8f1f2773110e9
poorna30646 <thotipoornachandra@gmail.com>
2024-12-11 09:58:38 +0530

    Add files via upload

---
+++ name_age.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class name_age
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String a=sc.next();
+        int b=sc.nextInt();
+        System.out.println("The name of the person is" +a +"and age is " +b);
+    }
+}
